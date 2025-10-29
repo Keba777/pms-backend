fbf1e865d32215859339ef1fab3bff35c949b2dd
Kibreab Aklilu <Akkeba37@gmail.com>
2025-10-29 22:10:51 +0300

    fix: Update task route to use the correct updateTaskActuals API for actuals

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { createTask, getAllTasks, getTaskById, updateTask, deleteTask } from "../controllers/task.controller";
+import { createTask, getAllTasks, getTaskById, updateTask, deleteTask, updateTaskActuals } from "../controllers/task.controller";
 
 const router = express.Router();
 
 router.route("/").post(createTask).get(getAllTasks);
 router.route("/:id").get(getTaskById).put(updateTask).delete(deleteTask);
-router.route("/:id/actuals").put(updateTask);
+router.route("/:id/actuals").put(updateTaskActuals);
 
 export default router;
