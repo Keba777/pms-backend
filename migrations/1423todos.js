3ba338470c5ea1639a98e615f6b4b2d9b8b74302
Kibreab Aklilu <akkeba37@gmail.com>
2026-01-14 16:16:04 +0300

    fix: fixing the todos type errors

---
+++ migrations/1423todos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      'ALTER TABLE "todos" ALTER COLUMN "target" TYPE VARCHAR(255)[] USING "target"::VARCHAR(255)[];'
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Attempt to revert back to string, joining array elements with comma if possible, or just casting to text.
+    // This is a best-effort revert.
+    await queryInterface.sequelize.query(
+      'ALTER TABLE "todos" ALTER COLUMN "target" TYPE VARCHAR(255) USING array_to_string("target", \',\');'
+    );
+  }
+};
